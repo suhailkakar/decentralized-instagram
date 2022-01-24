7f2f804aeb1ce6ae21b0a99894004b8dd8a66b71
Suhail Kakar <56798748+suhailkakar@users.noreply.github.com>
2022-01-24 11:36:23 +0000

    Removed bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,10 @@
-import React, { Component } from 'react';
-import Web3 from 'web3';
-import Identicon from 'identicon.js';
-import './App.css';
-import Decentragram from '../abis/Decentragram.json'
-import Navbar from './Navbar'
-import Main from './Main'
+import { View, Text } from "react-native";
+import React from "react";
 
-
-class App extends Component {
-
-  constructor(props) {
-    super(props)
-    this.state = {
-      account: '',
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        <Navbar account={this.state.account} />
-        { this.state.loading
-          ? <div id="loader" className="text-center mt-5"><p>Loading...</p></div>
-          : <Main
-            // Code...
-            />
-          }
-        }
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <View>
+      <Text>dasdsa</Text>
+    </View>
+  );
 }
-
-export default App;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,23 +1,25 @@
-import React, { Component } from 'react';
-import Identicon from 'identicon.js';
+import React, { Component } from "react";
+import Identicon from "identicon.js";
 
 class Main extends Component {
-
   render() {
     return (
       <div className="container-fluid mt-5">
         <div className="row">
-          <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '500px' }}>
+          <main
+            role="main"
+            className="col-lg-12 ml-auto mr-auto"
+            style={{ maxWidth: "500px" }}
+          >
             <div className="content mr-auto ml-auto">
               <p>&nbsp;</p>
               <h1 className="d-4">Edit this file in App.js!</h1>
-                
-                {/* Code ... */}
+
+              {/* Code ... */}
 
               <p>&nbsp;</p>
-                
-                {/* Code ... */}
 
+              {/* Code ... */}
             </div>
           </main>
         </div>
